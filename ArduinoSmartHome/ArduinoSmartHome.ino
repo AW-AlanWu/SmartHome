53cd34e2856b337eca52163824b8001c9a3ed691
AW-AlanWu <alan.92925@gmail.com>
2020-09-28 15:12:12 +0800

    完成一顆0.5秒閃一次的警示燈

---
+++ ArduinoSmartHome/ArduinoSmartHome.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(12, 1);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(12, 1);
+  delay(500);
+  digitalWrite(12, 0);
+  delay(500);
+}
