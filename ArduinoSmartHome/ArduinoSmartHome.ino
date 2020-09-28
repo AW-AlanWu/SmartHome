4dea3d4a8ff8c25a8f936018d1a980489b6c2123
AW-AlanWu <alan.92925@gmail.com>
2020-09-28 16:03:36 +0800

    修改為斷線時才發警報

--- ArduinoSmartHome/ArduinoSmartHome.ino
+++ ArduinoSmartHome/ArduinoSmartHome.ino
@@ -1,17 +1,21 @@
 void setup()
 {
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(7, OUTPUT);
+pinMode(12, OUTPUT);
+pinMode(11, OUTPUT);
+pinMode(7, OUTPUT);
+pinMode(2, INPUT);
+Serial.begin(9600);
 }
 void loop()
 {
-  digitalWrite(12, HIGH);
-  digitalWrite(11, LOW);
-  tone(7, 750, 500);
-  delay(200);
-  digitalWrite(12, LOW);
-  digitalWrite(11, HIGH);
-  tone(7, 100, 300);
-  delay(500);
+if(digitalRead(2)==HIGH) {
+digitalWrite(12, HIGH);
+digitalWrite(11, LOW);
+tone(7, 853, 500);
+delay(250);
+digitalWrite(12, LOW);
+digitalWrite(11, HIGH);
+tone(7, 960, 500);
+delay(500);
+}
 }
